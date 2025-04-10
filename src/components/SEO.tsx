5931daca429b79cf7e92d5a00f6b560cd91285ec
AngeljoNG <joelle@nguyen.eu>
2025-04-10 13:44:43 +0200

    Deploiement du 09/04/25

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -16,15 +16,22 @@ export function SEO({
     // Mots-clés principaux
     "psychologue",
     "thérapeute",
-    "graphothérapeute",
+    "graphothérapeute", 
     "psychologue clinicienne",
+    "psychologue Faimes",
+    "psychologue Liège",
+    "psychologue enfants",
+    "psychologue adolescents",
 
     // Localisation
     "Faimes",
     "Liège",
-    "Waremme",
+    "Waremme", 
     "Wallonie",
     "province de Liège",
+    "Belgique",
+    "consultation Faimes",
+    "consultation Liège",
 
     // Spécialités
     "TCC",
@@ -33,6 +40,11 @@ export function SEO({
     "thérapies cognitivo comportementales",
     "graphothérapie",
     "rééducation écriture",
+    "thérapie brève",
+    "thérapie comportementale", 
+    "thérapie cognitive",
+    "hypnose thérapeutique",
+    "EMDR",
 
     // Problématiques
     "anxiété",
@@ -41,22 +53,46 @@ export function SEO({
     "dysgraphie",
     "stress",
     "difficultés scolaires",
+    "traumatisme",
+    "phobie",
+    "trouble anxieux",
+    "burn out",
+    "confiance en soi",
+    "troubles du sommeil",
+    "troubles alimentaires",
 
     // Services
     "consultation psychologique",
     "suivi thérapeutique",
     "accompagnement enfants",
     "thérapie adolescents",
-    "psychologue adultes"
+    "psychologue adultes",
+    "bilan graphomoteur",
+    "rééducation graphomotrice",
+    "psychothérapie",
+    "thérapie en ligne",
+    "consultation à distance",
+
+    // Collaborations
+    "DysMoi",
+    "REALISM", 
+    "réseau psychologues",
+    "psychologue première ligne",
+    "PPL",
+    "troubles DYS",
+    "neurodiversité"
   ],
   image = "/pearl-profile.jpg",
-  url = "https://pearl-nguyen.eu"
+  url = "https://pearlnguyenduy.be"
 }: SEOProps) {
   const siteTitle = "Pearl Nguyen Duy";
   const fullTitle = title === siteTitle ? title : `${title} | ${siteTitle}`;
 
   return (
     <Helmet>
+      {/* Google Search Console Verification */}
+      <meta name="google-site-verification" content="v46zz3bDohbW2Nvzox6EPgtcAt0mF0cJufQn0QL_VGk" />
+
       {/* Balises meta de base */}
       <title>{fullTitle}</title>
       <meta name="description" content={description} />
@@ -68,6 +104,8 @@ export function SEO({
       <meta property="og:title" content={fullTitle} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
+      <meta property="og:site_name" content="Pearl Nguyen Duy" />
+      <meta property="og:locale" content="fr_BE" />
 
       {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
@@ -77,10 +115,12 @@ export function SEO({
       <meta name="twitter:image" content={image} />
 
       {/* Autres balises meta importantes */}
-      <meta name="robots" content="index, follow" />
+      <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
       <meta name="language" content="French" />
       <meta name="revisit-after" content="7 days" />
       <meta name="author" content="Pearl Nguyen Duy" />
+      <meta name="theme-color" content="#4a6741" />
+      <meta name="format-detection" content="telephone=yes" />
 
       {/* Balises spécifiques à la localisation */}
       <meta name="geo.region" content="BE-WLG" />
@@ -90,6 +130,7 @@ export function SEO({
 
       {/* Liens canoniques et alternates */}
       <link rel="canonical" href={url} />
+      <link rel="alternate" hrefLang="fr-BE" href={url} />
     </Helmet>
   );
 }
